99d18e8867795fa7381152f4120304e4f1b3671b
Patrik Kraif <kraifpatrik@gmail.com>
2023-09-23 09:55:33 +0200

    Documenting btSequentialImpulseConstraintSolver exports

--- src/GMBullet/exports_btSequentialImpulseConstraintSolver.cpp
+++ src/GMBullet/exports_btSequentialImpulseConstraintSolver.cpp
@@ -3,19 +3,27 @@
 /// @func btSequentialImpulseConstraintSolver_create()
 ///
 /// @desc
+/// Creates a new instance of a Bullet Physics sequential impulse constraint
+/// solver.
 ///
-/// @return {Pointer}
+/// @return {Pointer} A pointer to the newly created instance of
+/// btSequentialImpulseConstraintSolver.
 YYEXPORT void btSequentialImpulseConstraintSolver_create(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
 	result.kind = VALUE_PTR;
 	result.ptr = new btSequentialImpulseConstraintSolver();
 }
+
 /// @func btSequentialImpulseConstraintSolver_destroy(sequentialImpulseConstraintSolver)
 ///
 /// @desc
+/// Destroys an instance of a Bullet Physics sequential impulse constraint
+/// solver.
 ///
 /// @param {Pointer} sequentialImpulseConstraintSolver
+///     A pointer to the btSequentialImpulseConstraintSolver object to be
+///     destroyed.
 YYEXPORT void btSequentialImpulseConstraintSolver_destroy(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
