30c435da7f500acad97ed3dd04e0b1ce4be09040
Patrik Kraif <kraifpatrik@gmail.com>
2023-09-23 10:03:21 +0200

    Documenting btTransform exports

--- src/GMBullet/exports_btTransform.cpp
+++ src/GMBullet/exports_btTransform.cpp
@@ -3,8 +3,9 @@
 /// @func btTransform_create()
 ///
 /// @desc
+/// Creates a new instance of a Bullet Physics transform.
 ///
-/// @return {Pointer}
+/// @return {Pointer} A pointer to the newly created instance of btTransform.
 YYEXPORT void btTransform_create(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
@@ -15,8 +16,10 @@ YYEXPORT void btTransform_create(
 /// @func btTransform_destroy(transform)
 ///
 /// @desc
+/// Destroys an instance of a Bullet Physics transform.
 ///
 /// @param {Pointer} transform
+///     A pointer to the btTransform object to be destroyed.
 YYEXPORT void btTransform_destroy(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
@@ -38,9 +41,10 @@ YYEXPORT void btTransform_setIdentity(
 /// @func btTransform_getOrigin(transform, outVector3)
 ///
 /// @desc
+/// Sets the transform to the identity transformation.
 ///
 /// @param {Pointer} transform
-/// @param {Pointer} outVector3
+///     A pointer to the btTransform object to be set to identity.
 YYEXPORT void btTransform_getOrigin(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
@@ -52,9 +56,12 @@ YYEXPORT void btTransform_getOrigin(
 /// @func btTransform_setOrigin(transform, origin)
 ///
 /// @desc
+/// Sets the origin of the transform using a btVector3.
 ///
 /// @param {Pointer} transform
+///     A pointer to the btTransform object.
 /// @param {Pointer} origin
+///     A pointer to the btVector3 representing the origin.
 YYEXPORT void btTransform_setOrigin(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
@@ -66,11 +73,16 @@ YYEXPORT void btTransform_setOrigin(
 /// @func btTransform_setOriginXYZ(transform, x, y, z)
 ///
 /// @desc
+/// Sets the origin of the transform using individual x, y, and z coordinates.
 ///
 /// @param {Pointer} transform
+///     A pointer to the btTransform object.
 /// @param {Real} x
+///     The x-coordinate of the origin.
 /// @param {Real} y
+///     The y-coordinate of the origin.
 /// @param {Real} z
+///     The z-coordinate of the origin.
 YYEXPORT void btTransform_setOriginXYZ(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
@@ -84,9 +96,13 @@ YYEXPORT void btTransform_setOriginXYZ(
 /// @func btTransform_getRotation(transform, outQuaternion)
 ///
 /// @desc
+/// Retrieves the rotation of the transform as a quaternion and stores it in the
+/// specified btQuaternion.
 ///
 /// @param {Pointer} transform
+///     A pointer to the btTransform object.
 /// @param {Pointer} outQuaternion
+///     A pointer to a btQuaternion to store the rotation.
 YYEXPORT void btTransform_getRotation(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
@@ -98,9 +114,12 @@ YYEXPORT void btTransform_getRotation(
 /// @func btTransform_setRotation(transform, quaternion)
 ///
 /// @desc
+/// Sets the rotation of the transform using a btQuaternion.
 ///
 /// @param {Pointer} transform
+///     A pointer to the btTransform object.
 /// @param {Pointer} quaternion
+///     A pointer to the btQuaternion representing the rotation.
 YYEXPORT void btTransform_setRotation(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
@@ -109,15 +128,20 @@ YYEXPORT void btTransform_setRotation(
 	transform->setRotation(quaternion);
 }
 
-/// @func btTransform_setRotationXYZ(transform, yaw, pitch, roll)
+/// @func btTransform_setRotationEuler(transform, yaw, pitch, roll)
 ///
 /// @desc
+/// Sets the rotation of the transform using Euler angles (yaw, pitch, roll).
 ///
 /// @param {Pointer} transform
+///     A pointer to the btTransform object.
 /// @param {Real} yaw
+///     The yaw angle in radians.
 /// @param {Real} pitch
+///     The pitch angle in radians.
 /// @param {Real} roll
-YYEXPORT void btTransform_setRotationXYZ(
+///     The roll angle in radians.
+YYEXPORT void btTransform_setRotationEuler(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
 	auto transform = (btTransform*)YYGetPtr(arg, 0);
@@ -130,9 +154,13 @@ YYEXPORT void btTransform_setRotationXYZ(
 /// @func btTransform_getMatrix(transform, outMatrix)
 ///
 /// @desc
+/// Retrieves the transformation matrix of the btTransform and stores it in the
+/// specified array.
 ///
 /// @param {Pointer} transform
-/// @param {Array<real>} outMatrix
+///     A pointer to the btTransform object.
+/// @param {Array<Real>} outMatrix
+///     An array to store the transformation matrix (16 elements).
 YYEXPORT void btTransform_getMatrix(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
@@ -153,9 +181,13 @@ YYEXPORT void btTransform_getMatrix(
 /// @func btTransform_setFromMatrix(transform, matrix)
 ///
 /// @desc
+/// Sets the transformation of the btTransform using the provided transformation
+/// matrix.
 ///
 /// @param {Pointer} transform
-/// @param {Array<real>} matrix
+///     A pointer to the btTransform object.
+/// @param {Array<Real>} matrix
+///     An array representing the transformation matrix (16 elements).
 YYEXPORT void btTransform_setFromMatrix(
 	RValue& result, CInstance* self, CInstance* other, int argc, RValue* arg)
 {
